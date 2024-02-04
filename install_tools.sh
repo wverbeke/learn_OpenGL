d79b6ff36293c0c4e529cb7899f745963b0d269c
Willem Verbeke <willem_verbeke@hotmail.com>
2024-02-04 19:50:11 +0100

    Requirements

---
+++ install_tools.sh
@@ -0,0 +1,9 @@
+sudo apt install cmake
+sudo apt install libxrandr-dev 
+sudo apt install libxinerama-dev 
+sudo apt install libxcursor-dev 
+sudo apt install libxi-dev 
+
+# Go to https://www.glfw.org/ and download the source code.
+# There run cmake, followed by make install.
+# Now linker flags provided in compile.py should work.
